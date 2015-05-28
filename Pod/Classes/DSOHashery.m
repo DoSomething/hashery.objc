422add5b3faee67b1f62ae2c3bbffa1233ea52f5
Tong Xiang <tong.xiang2@gmail.com>
2015-05-28 18:33:06 -0400

    datatype changes

--- Pod/Classes/DSOHashery.m
+++ Pod/Classes/DSOHashery.m
@@ -212,7 +212,7 @@
         result += n * pow(_base, power);
     }
     
-    NSLog(@"original base-10 number is: %lg", (long)result);
+    NSLog(@"original base-10 number is: %ld", (long)result);
     return result;
 }
 
